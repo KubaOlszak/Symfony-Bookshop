6c0c45660164d70beb17d29f3c674bee138fce61
Kuba OLSZAK <analogband@gmail.com>
2023-06-09 08:33:46 +0200

    Creation formulaire

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -2,17 +2,38 @@
 
 namespace App\Controller;
 
+use App\Entity\Book;
+use App\Form\BookType;
+use App\Repository\BookRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Forms;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BookController extends AbstractController
 {
-    #[Route('/book', name: 'app_book')]
-    public function index(): Response
+    #[Route('/books', name: 'app_books')]
+    public function index(BookRepository $bookRepository): Response
     {
         return $this->render('book/index.html.twig', [
-            'controller_name' => 'BookController',
+            'books' => $bookRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/books/{id<\d+>}', name: 'app_books_edit')]
+    public function edit(Request $request, Book $book, BookRepository $bookRepository): Response
+    {
+        $form = $this->createForm(BookType::class);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $book = $form->getData();
+            $bookRepository->save($book);
+        }
+
+        return $this->render('book/edit.html.twig', [
+            'form' => $form,
         ]);
     }
 }
